d8adeffdd3dabaaf5de4fb1c7225a74d0295031d
Lucas Carney <carney071@gmail.com>
2014-03-31 13:45:32 -0500

    updates to the styles and the some java script. Making the redditn app will be helpful for pulling the weather data.

--- Resources/iphone/alloy/controllers/index.js
+++ Resources/iphone/alloy/controllers/index.js
@@ -1,7 +1,4 @@
 function Controller() {
-    function doClick() {
-        alert($.label.text);
-    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
     arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -9,57 +6,37 @@ function Controller() {
     arguments[0] ? arguments[0]["__itemTemplate"] : null;
     var $ = this;
     var exports = {};
-    var __defers = {};
-<<<<<<< HEAD
-    $.__views.index = Ti.UI.createWindow({
-        backgroundColor: "white",
-        id: "index"
-    });
-    $.__views.index && $.addTopLevelView($.__views.index);
-=======
     var __alloyId0 = [];
     $.__views.clothingWindow = Ti.UI.createWindow({
+        backgroundColor: "#00007A",
         id: "clothingWindow"
     });
->>>>>>> b6ac7dc43ed77f43defd06a862aeabf741cd3233
-    $.__views.label = Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
-        color: "#000",
-        text: "Hello, World",
-        id: "label"
-    });
-<<<<<<< HEAD
-    $.__views.index.add($.__views.label);
-    doClick ? $.__views.label.addEventListener("click", doClick) : __defers["$.__views.label!click!doClick"] = true;
-=======
-    $.__views.clothingWindow.add($.__views.label);
-    doClick ? $.__views.label.addEventListener("click", doClick) : __defers["$.__views.label!click!doClick"] = true;
-    $.__views.__alloyId1 = Ti.UI.createTab({
+    $.__views.appTab2 = Ti.UI.createTab({
         window: $.__views.clothingWindow,
         title: "Clothing",
+        id: "appTab2"
+    });
+    __alloyId0.push($.__views.appTab2);
+    $.__views.weatherWindow = Ti.UI.createWindow({
+        backgroundColor: "#fff",
+        id: "weatherWindow"
+    });
+    $.__views.__alloyId1 = Ti.UI.createTab({
+        tabsBackgroundColor: "green",
+        window: $.__views.weatherWindow,
+        title: "Weather",
         id: "__alloyId1"
     });
     __alloyId0.push($.__views.__alloyId1);
-    $.__views.weatherWindow = Ti.UI.createWindow({
-        id: "weatherWindow"
-    });
-    $.__views.__alloyId2 = Ti.UI.createTab({
-        window: $.__views.weatherWindow,
-        title: "Weather",
-        id: "__alloyId2"
-    });
-    __alloyId0.push($.__views.__alloyId2);
     $.__views.index = Ti.UI.createTabGroup({
+        tabsBackgroundColor: "blue",
         tabs: __alloyId0,
         id: "index"
     });
     $.__views.index && $.addTopLevelView($.__views.index);
->>>>>>> b6ac7dc43ed77f43defd06a862aeabf741cd3233
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.index.open();
-    __defers["$.__views.label!click!doClick"] && $.__views.label.addEventListener("click", doClick);
     _.extend($, exports);
 }
 
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,27 @@ function doClick(e) {
     alert($.label.text);
 }
 
+
+/*
+var url= "http://www.weather.com";
+var client = Ti.Network.HTTPClient({
+	onload : function(e){
+		Ti.API.info();
+		alert('success');
+	},
+	onerror : function(e){
+		Ti.API.debug(e.error);
+		alert('There has been an error');
+	},
+	timeout : 5000 //in Milliseconds
+});
+
+*/
+
+
+
+
+
+
+
 $.index.open();
